adbd73a7766a023744c36c3dbf822d3992797962
TK17250 <TXK285@gmail.com>
2024-06-13 00:11:02 +0700

    Add auto scroll ground.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance {
+        get;
+        private set;
+    }
+
+    public float initialGameSpeed = 5f;
+    public float gameSpeedIncrement = 0.1f;
+    public float gameSpeed {
+        get;
+        private set;
+    }
+
+    public void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else {
+            DestroyImmediate(gameObject);
+        }
+    }
+
+    private void OnDestroy() {
+        if (instance == this) {
+            instance = null;
+        }
+    }
+
+    private void Start() {
+        NewGame();
+    }
+
+    private void NewGame() {
+        gameSpeed = initialGameSpeed;
+    }
+
+    private void Update() {
+        gameSpeed += gameSpeedIncrement * Time.deltaTime;
+    }
+}
