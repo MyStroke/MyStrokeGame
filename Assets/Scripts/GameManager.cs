59817208b87e1e1595912461da84ac0eaa9179ba
TK17250 <TXK285@gmail.com>
2024-06-17 23:25:21 +0700

    Add UI and score real time.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,23 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager instance {
-        get;
-        private set;
-    }
+    public static GameManager instance { get; private set; }
 
     public float initialGameSpeed = 5f;
     public float gameSpeedIncrement = 1f;
-    public float gameSpeed {
-        get;
-        private set;
-    }
+    public float gameSpeed { get; private set; }
+
+    // Get Game Object
+    public TextMeshProUGUI gameOverText; // Game Over
+    public TextMeshProUGUI scoreUpdate; // Score
+    public Button retryButton; // Retry Button
 
     private Player player;
     private Spawner spawner;
+
+    private float score;
 
     // Awake
     public void Awake() {
@@ -44,18 +47,21 @@ public class GameManager : MonoBehaviour
     }
 
     // New Game
-    private void NewGame() {
+    public void NewGame() {
         Monsters[] monsters = FindObjectsOfType<Monsters>();
 
         foreach (var monster in monsters) {
             Destroy(monster.gameObject);
         }
 
+        score = 0f;
         gameSpeed = initialGameSpeed;
         enabled = true;
 
         player.gameObject.SetActive(true);
         spawner.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(false);
+        retryButton.gameObject.SetActive(false);
     }
 
     // Game Over
@@ -65,5 +71,13 @@ public class GameManager : MonoBehaviour
 
         player.gameObject.SetActive(false);
         spawner.gameObject.SetActive(false);
+        gameOverText.gameObject.SetActive(true);
+        retryButton.gameObject.SetActive(true);
+    }
+
+    // Update
+    private void Update() {
+        score += gameSpeed * Time.deltaTime;
+        scoreUpdate.text = $"{score:0}";
     }
 }
